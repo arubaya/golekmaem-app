6bc76a4da7ea445941ce6330efdfdc742e3c0932
Tsabit Arubaya <61440656+arubaya@users.noreply.github.com>
2022-04-09 11:35:00 +0700

    remove bundle analyzer

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin');
 const ImageminWebpackPlugin = require('imagemin-webpack-plugin').default;
 const ImageminMozjpeg = require('imagemin-mozjpeg');
@@ -86,6 +86,6 @@ module.exports = {
       ],
     }),
     new CleanWebpackPlugin(),
-    new BundleAnalyzerPlugin(),
+//     new BundleAnalyzerPlugin(),
   ],
 };
